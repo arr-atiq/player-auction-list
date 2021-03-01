1acc19da16ac5a3aaf08327381f8489f434582bd
aar-atiq <aar.msdeveloper01@gmail.com>
2021-03-01 12:48:20 +0600

    added bootstrap

--- src/Components/DisplayInformation/DisplayInformation.js
+++ src/Components/DisplayInformation/DisplayInformation.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './DisplayInformation.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFileSignature, faFlagCheckered, faUserCog, faDollarSign, faPlusSquare} from '@fortawesome/free-solid-svg-icons'
+import Button from 'react-bootstrap/Button';
 
 const DisplayInformation = (props) => {
     const {name, nationality, salary, image, team} = props.playerInfo;
@@ -10,12 +11,12 @@ const DisplayInformation = (props) => {
             <div className="player-data">
                 <img src={image} alt=""/>
                 <h3><FontAwesomeIcon icon={faFileSignature} /> {name}</h3>
-                <h4><FontAwesomeIcon icon={faFlagCheckered} /> {nationality}</h4>
-                <h4><FontAwesomeIcon icon={faUserCog} /> {team}</h4>
-                <h4><FontAwesomeIcon icon={faDollarSign} />{salary}</h4>
-                <button className="main-btn"
+                <h3><FontAwesomeIcon icon={faFlagCheckered} /> {nationality}</h3>
+                <h3><FontAwesomeIcon icon={faUserCog} /> {team}</h3>
+                <h3><FontAwesomeIcon icon={faDollarSign} />{salary}</h3>
+                <Button variant="success addbtn"
                 onClick={()=> props.handleAddPlayer(props.playerInfo)}
-                ><FontAwesomeIcon icon={faPlusSquare} /> Select Me</button>
+                ><FontAwesomeIcon icon={faPlusSquare} /> Select Me</Button>
             </div>
             
         </div>
