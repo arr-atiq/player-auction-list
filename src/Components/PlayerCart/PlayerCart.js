9bc6db83a6862a590eeeae6adccea152855184b9
aar-atiq <aar.msdeveloper01@gmail.com>
2021-03-01 11:08:46 +0600

    all cart state added

--- src/Components/PlayerCart/PlayerCart.js
+++ src/Components/PlayerCart/PlayerCart.js
@@ -3,14 +3,30 @@ import './PlayerCart.css';
 
 const PlayerCart = (props) => {
     const newPlayerCart = props.playerCart;
+
+    let totalBudget = 0;
+    let playerPrimeInfo = [];
+    for (let i = 0; i < newPlayerCart.length; i++) {
+        const getCart = newPlayerCart[i];
+        totalBudget = totalBudget+getCart.salary;
+        const playerPrimeInfo1 = getCart.name;
+        const playerPrimeInfo2 = getCart.salary;
+        const allInformation = playerPrimeInfo1 + "- $"+playerPrimeInfo2;
+        playerPrimeInfo.push(allInformation);
+    }
     return (
         <div className="cart-show">
             <div className="cart-info">
                 <h1 className="title"><span className="secondH">Player</span> Budget</h1>
             </div>
-            <div>
+            <div className="cart-mainInfo">
                 <h3>Player Added: {newPlayerCart.length}</h3>
-                <h3>Total Badget:</h3>
+                <h3>Total Budget: ${totalBudget}</h3>
+            </div>
+            <div>
+                {
+                    playerPrimeInfo.map(info => <div className="info-div">{info}</div> )
+                }
             </div>
         </div>
     );
